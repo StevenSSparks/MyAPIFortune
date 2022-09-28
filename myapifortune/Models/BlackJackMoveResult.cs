13bf95afbb1c35d0106496908faab9c1c2762a91
Steve Sparks <15949542+StevenSSparks@users.noreply.github.com>
2022-09-27 22:32:31 -0500

    Update BlackJackMoveResult.cs

--- myapifortune/Models/BlackJackMoveResult.cs
+++ myapifortune/Models/BlackJackMoveResult.cs
@@ -5,8 +5,12 @@
         public string GameId { get; set; } = string.Empty;
         public string PlayerWins { get; set; } = string.Empty;
         public string ComputerWins { get; set; } = string.Empty;
-        public List<Card> PlayerCards { get; set; } = new List<Card>();
-        public List<Card> ComputerCards { get; set; } = new List<Card>();
+        public string PlayerPoints { get; set; } = string.Empty;
+        public List<Card> ComputerCards { get; set; } = new();
+        public List<Card> PlayerCards { get; set; } = new();
+        public List<Card> PlayerSplitCards { get; set; } = new();
+        public bool PlayerCardsActive { get; set; } = false;
+        public bool PlayerSplitActive { get; set; } = false;
         public string Message { get; set; } = string.Empty;
     }
 }
